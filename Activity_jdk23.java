65373737a66b98252d2b7b58c6b10e4d38ffc613
GHuser470097 <2002tutan@gmail.com>
2024-12-02 13:43:18 +0530

    Add files via upload

---
+++ Activity_jdk23.java
@@ -0,0 +1,18 @@
+class Activity_jdk23
+{
+	public static void main(String []args)
+	{
+		int sub1 = 88;
+		int sub2 = 54;
+		int sub3 = 12;
+		int sub4 = 67;
+		int sub5 = 0;
+		
+		double total;
+		total = sub1+sub2+sub3+sub4+sub5;
+		System.out.println("Total : "+total);
+		double percent;
+		percent = (total/500)*100;
+		System.out.println("Percentage : "+percent);
+	}
+}
